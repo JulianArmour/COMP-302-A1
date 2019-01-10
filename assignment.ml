eea10d0107e3941599c770043ae7b06e656132e7
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-01-10 16:27:08 -0500

    Completed cube_root function

--- assignment.ml
+++ assignment.ml
@@ -6,3 +6,14 @@ let mysqrt (x:float) =
         | false -> aux ((g +. x /. g) /. 2.0) x
     in
     aux 1.0 x
+
+let g'_cube g x = (2.0 *. g +. x /. (g ** 2.0)) /. 3.0
+
+let cube_root (x:float) =
+    let rec aux g x =
+        let is_close = close ((cube g),x) in
+        match is_close with
+        | true -> g
+        | false -> aux (g'_cube g x) x
+    in
+    aux 1.0 x
