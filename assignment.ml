5f9e5985711a048d3eb920f17d75023463efd592
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-01-10 16:03:50 -0500

    Initial mysqrt algo

---
+++ assignment.ml
@@ -0,0 +1,8 @@
+let mysqrt (x:float) =
+    let rec aux g x =
+        let is_close = close (square g) x in
+        match is_close with
+        | true -> g
+        | false -> aux ((g +. x /. g) /. 2.0) x
+    in
+    aux 1.0 x
